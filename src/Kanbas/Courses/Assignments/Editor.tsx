b16415abafb02b62dffe1c78ba084e26c792027b
JennyjieyiZ <zhou.jie@northeastern.edu>
2024-09-19 22:22:00 -0400

    textarea

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -3,7 +3,7 @@ export default function AssignmentEditor() {
       <div id="wd-assignments-editor">
         <label htmlFor="wd-name">Assignment Name</label>
         <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
-        <textarea id="wd-description">
+        <textarea id="wd-description" cols={50} rows={10}>
           The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following:
           Your full name and section 
           Links to each of the lab assignments Link to the Kanbas application
