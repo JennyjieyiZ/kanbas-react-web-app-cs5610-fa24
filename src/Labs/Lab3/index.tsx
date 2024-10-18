5634b62baa0d5301b6e3e7f42f96d0b9a300cafc
JennyjieyiZ <zhou.jie@northeastern.edu>
2024-10-17 23:20:51 -0400

    a3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -28,7 +28,6 @@ import Classes from "./Classes";
 import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
-import AddPathParameters from "./AddPathParameters";
 import PathParameters from "./PathParameters";
 import FindIndex from "./FindIndex";
 
