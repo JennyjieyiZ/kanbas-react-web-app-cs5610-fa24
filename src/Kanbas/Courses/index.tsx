b74bc0e0c1652db7f158019ff9634dea56856d11
JennyjieyiZ <zhou.jie@northeastern.edu>
2024-12-11 05:04:07 -0500

    a6

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -29,6 +29,9 @@ export default function Courses({ courses }: { courses: any[]; }) {
               <Route path="Assignments" element={<Assignments />} />
               <Route path="Assignments/:aid" element={<AssignmentEditor />} />
               <Route path="People" element={<PeopleTable />} />
+              
+                        
+                        
             </Routes>
             </div></div>
 
